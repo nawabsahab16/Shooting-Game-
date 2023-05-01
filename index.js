d7c6479d478707cf6b2d2889e08eb1a2c5af56dc
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-05-01 14:57:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ const canvas = document.createElement("canvas");
 
 document.querySelector(".MyGame").appendChild(canvas);
 
-const ct= canvas.getContext("2d");
+canvas.height = innerHeight;
+
+canvas.width = innerWidth;
+
+const context = canvas.getContext("2d");
+
+context.beginPath();
+context.arc(100, 150, 10, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+context.stroke();
+
+context.beginPath();
+context.arc(150, 150, 10, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+context.stroke();
+
+context.beginPath();
+context.arc(100, 100, 10, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+context.stroke();
+
+context.beginPath();
+context.arc(150, 100, 10, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+context.stroke();
+
